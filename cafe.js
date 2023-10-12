d2765eb4e066bffa574b062439f3e28968b5b5ce
Haram1992 <144920268+Haram1992@users.noreply.github.com>
2023-10-12 22:29:20 +0700

    Cafe and cafe detail 01 (#2)

---
+++ cafe.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//     $(".go-detail").click(function(){
+//         window.location.href("./cafeDetail/cafeDetail01.html")
+//     })
+// })
+function goDetail(){
+     window.location = "./cafeDetail/cafeDetail01.html"
+
+}
