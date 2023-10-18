a61d69a410beccaa8cebfe88b569b07d7cb0cd29
Haram1992 <144920268+Haram1992@users.noreply.github.com>
2023-10-19 00:41:56 +0700

    Update cafe function

--- cafe.js
+++ cafe.js
@@ -3,7 +3,64 @@
 //         window.location.href("./cafeDetail/cafeDetail01.html")
 //     })
 // })
-function goDetail(){
-     window.location = "./cafeDetail/cafeDetail01.html"
+let cafes = [
+     { 
+      id: 'cafe01',
+      name: "cafe 01",
+      price: "31.000",
+      img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
+   },
+    { 
+      id: 'cafe02',
+      name: "cafe 02",
+        price: "32.000",
+      img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
+   },
+   { 
+      id: 'cafe03',
+      name: "cafe 03",
+        price: "33.000",
+      img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
+   },
+   { 
+      id: 'cafe04',
+      name: "cafe 04",
+        price: "34.000",
+      img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
+   },
+   { 
+      id: 'cafe05',
+      name: "cafe 05",
+        price: "35.000",
+      img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
+   },
+   { 
+      id: 'cafe06',
+      name: "cafe 06",
+        price: "36.000",
+      img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
+   },
+   { 
+      id: 'cafe07',
+      name: "cafe 07",
+        price: "37.000",
+      img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
+   },
+   { 
+      id: 'cafe08',
+      name: "cafe 08",
+        price: "38.000",
+      img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
+   },
+   ]
+   const product = document.querySelectorAll('.products')
+   product.forEach((x, i)=> {
+     x.querySelector('img').setAttribute('src', cafes[i].img) 
+     x.querySelector('img').setAttribute('id', cafes[i].id) 
+     x.querySelector('.card-text').innerText = cafes[i].name
+     x.querySelector('.price').innerText = cafes[i].price
+   })
 
+function goDetail(event){
+     window.location = `./cafeDetail/cafeDetail01.html?id=${event.id}`;
 }
--- cafeDetail/cafeDetail.js
+++ cafeDetail/cafeDetail.js
@@ -1,61 +1,58 @@
-let cafe = [
-   { 
-    id: 1,
-    name: "cafe 01",
-    price: "31.000",
-    img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
-},
+let cafes = [
   { 
-    id: 2,
-    name: "cafe 02",
-      price: "32.000",
-    img: ""
+   id: 'cafe01',
+   name: "cafe 01",
+   price: "31.000",
+   img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
+},
+ { 
+   id: 'cafe02',
+   name: "cafe 02",
+     price: "32.000",
+   img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
 },
 { 
-    id: 3,
-    name: "cafe 03",
-      price: "33.000",
-    img: ""
+   id: 'cafe03',
+   name: "cafe 03",
+     price: "33.000",
+   img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
 },
 { 
-    id: 4,
-    name: "cafe 04",
-      price: "34.000",
-    img: ""
+   id: 'cafe04',
+   name: "cafe 04",
+     price: "34.000",
+   img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
 },
 { 
-    id: 5,
-    name: "cafe 05",
-      price: "35.000",
-    img: ""
+   id: 'cafe05',
+   name: "cafe 05",
+     price: "35.000",
+   img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
 },
 { 
-    id: 6,
-    name: "cafe 06",
-      price: "36.000",
-    img: ""
+   id: 'cafe06',
+   name: "cafe 06",
+     price: "36.000",
+   img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
 },
 { 
-    id: 7,
-    name: "cafe 07",
-      price: "37.000",
-    img: ""
+   id: 'cafe07',
+   name: "cafe 07",
+     price: "37.000",
+   img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
 },
 { 
-    id: 8,
-    name: "cafe 08",
-      price: "38.000",
-    img: ""
+   id: 'cafe08',
+   name: "cafe 08",
+     price: "38.000",
+   img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
 },
 ]
-// var randomProperty = function (obj) {
-//     var keys = Object.keys(obj);
-//     return obj[keys[ keys.length * Math.random() << 0]];
-// };
-// console.log(randomProperty(cafe))
 
-// var cafeName01 = document.getElementById("cafe01")
-// cafeName01.innerText = randomProperty(cafe).name
+const productDetail = cafes.find(x => x.id == window.location.search.slice(4))
+const elDetail = document.querySelector('.main-detail')
+elDetail.querySelector('.detail-title').innerText = productDetail.name
+
 function getRandom(list) {
     const res = []
     for (let x= 1; x<=3; x++) {
@@ -68,14 +65,10 @@ function getRandomElements(list, item = 6){
     return [...list].sort(() => Math.random()> 0.5 ? 1 : -1).slice(0,item)
 }
 
-console.log(getRandomElements(cafe, 6))
-var cafeName01 = document.getElementById("cafe01")
-cafeName01.innerText = getRandomElements(cafe,6)[0].name
-var cafeName02 = document.getElementById("cafe02")
-cafeName02.innerText = getRandomElements(cafe,6)[1].name
-var cafeName03 = document.getElementById("cafe03")
-cafeName03.innerText = getRandomElements(cafe,6)[2].name
-var cafeName04 = document.getElementById("cafe04")
-cafeName04.innerText = getRandomElements(cafe,6)[3].name
-var cafeName05 = document.getElementById("cafe05")
-cafeName05.innerText = getRandomElements(cafe,6)[4].name
+const newArr = getRandomElements(cafes, 6)
+for(var i = 0; i <= 5; i++) {
+  var cafeName01 = document.getElementById("cafe0"+(i+1))
+  console.log('document.querySelector(`#cafe0${(i+1)} img`)', document.querySelector(`#cafe0${(i+1)} img`));
+  // document.querySelector(`#cafe0${(i+1)} img`).setAttribute('msp', newArr[i].img)
+  cafeName01.querySelector('.card-text').innerText = newArr[i].name
+}
