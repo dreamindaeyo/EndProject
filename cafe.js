6eb0699df80aea0e5de817f6fba6265e2213b1bc
Dream <dreamnguyen1997@gmail.com>
2023-10-23 22:29:22 +0700

    commit 23/10

--- cafe.js
+++ cafe.js
@@ -6,59 +6,162 @@
 let cafes = [
      { 
       id: 'cafe01',
-      name: "cafe 01",
-      price: "31.000",
-      img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
+      name: "Cà Phê Đen Đá Hộp (14 gói x 16g)",
+      price: "58.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/1697446954_ca-phe-den-da-hop_6728d463f06942a1b691c1d40af8b8bd_large.jpg",
+      about: "Cà Phê Đen Đá hoà tan The Coffee House với 100% hạt cà phê Robusta mang đến hương vị mạnh cực bốc, đậm đắng đầy lôi cuốn, đúng gu người Việt.",
    },
     { 
       id: 'cafe02',
-      name: "cafe 02",
-        price: "32.000",
-      img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
+      name: "Cà Phê Đen Đá Túi (30 gói x 16g)",
+      price: "110.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/1697446642_ca-phe-den-da-tui_931eaea80eee49bcb8bb731dce2fd0c4_large.jpg",
+      about: "Cà Phê Đen Đá hoà tan The Coffee House với 100% hạt cà phê Robusta mang đến hương vị mạnh cực bốc, đậm đắng đầy lôi cuốn, đúng gu người Việt.",
    },
    { 
       id: 'cafe03',
-      name: "cafe 03",
-        price: "33.000",
-      img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
+      name: "Thùng Cà Phê Sữa Espresso",
+      price: "336.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/1669707271_24lon-espresso-no_8bebae682e4b479c951ddb16f483027e_large.jpg",
+      about: "Được sản xuất theo công nghệ Nhật, Cà Phê Sữa Espresso The Coffee House giữ trọn hương vị đậm đà của 100% cà phê Robusta nguyên chất quyện hoà cùng sữa béo thơm lừng. Bật nắp trải nghiệm ngay chất cà phê mạnh mẽ giúp sảng khoái tức thì, tuôn trào hứng khởi.",
    },
-   { 
+    { 
       id: 'cafe04',
-      name: "cafe 04",
-        price: "34.000",
-      img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
+      name: "Combo 6 Lon Cà Phê Sữa Espresso",
+      price: "84.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/1669707292_6lon-espresso-no_f3a81bf60969455db7f8082ce11a6a37_large.jpg",
+      about: "Được sản xuất theo công nghệ Nhật, Cà Phê Sữa Espresso The Coffee House giữ trọn hương vị đậm đà của 100% cà phê Robusta nguyên chất quyện hoà cùng sữa béo thơm lừng. Bật nắp trải nghiệm ngay chất cà phê mạnh mẽ giúp sảng khoái tức thì, tuôn trào hứng khởi.",
    },
    { 
       id: 'cafe05',
-      name: "cafe 05",
-        price: "35.000",
-      img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
+      name: "Cà Phê Rang Xay Original 1 Túi 1KG",
+      price: "235.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/1684727946_photo-2023-05-22-105837_d02ab9e6538042b1adb392a97fb60dd2_large.jpeg",
+      about: "Cà phê Original 1 của The Coffee House với 100% thành phần cà phê Robusta Đăk Lăk, vùng trồng cà phê ngon nhất Việt Nam. Bằng cách áp dụng kỹ thuật rang xay hiện đại, Cà phê Original 1 mang đến trải nghiệm tuyệt vời khi uống cà phê tại nhà với hương vị đậm đà truyền thống hợp khẩu vị của giới trẻ sành cà phê.",
    },
-   { 
+    { 
       id: 'cafe06',
-      name: "cafe 06",
-        price: "36.000",
-      img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
+      name: "Cà Phê Rang Xay Original 1 250g",
+      price: "60.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/1639648297_ca-phe-rang-xay-original-1-250gr_dad6e7c878df4207b49631eca111dbfd_large.jpg",
+      about: "Cà phê Original 1 của The Coffee House với thành phần chính cà phê Robusta Đắk Lắk, vùng trồng cà phê nổi tiếng nhất Việt Nam. Bằng cách áp dụng kỹ thuật rang xay hiện đại, Cà phê Original 1 mang đến trải nghiệm tuyệt vời khi uống cà phê tại nhà với hương vị đậm đà truyền thống hợp khẩu vị của giới trẻ sành cà phê.",
    },
    { 
       id: 'cafe07',
-      name: "cafe 07",
-        price: "37.000",
-      img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
+      name: "Cà Phê Sữa Đá Hòa Tan (10 gói x 22g)",
+      price: "44.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/cpsd-3in1_971575_689628f2b16b4792902ac107ae41bda3_large.jpg",
+      about: "Thật dễ dàng để bắt đầu ngày mới với tách cà phê sữa đá sóng sánh, thơm ngon như cà phê pha phin. Vị đắng thanh của cà phê hoà quyện với vị ngọt béo của sữa, giúp bạn luôn tỉnh táo và hứng khởi cho ngày làm việc thật hiệu quả.",
+   },
+    { 
+      id: 'cafe08',
+      name: "Cà Phê Sữa Đá Hòa Tan Túi 25x22G",
+      price: "99.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/1639648355_ca-phe-sua-da-hoa-tan-tui-25x22gr_527e9433f73c4f9da1ea398f2a94e8d2_large.jpg",
+      about: "Thật dễ dàng để bắt đầu ngày mới với tách cà phê sữa đá sóng sánh, thơm ngon như cà phê pha phin. Vị đắng thanh của cà phê hoà quyện với vị ngọt béo của sữa, giúp bạn luôn tỉnh táo và hứng khởi cho ngày làm việc thật hiệu quả.",
+   },
+     { 
+      id: 'cafe09',
+      name: "Cà Phê Hoà Tan Đậm Vị Việt (18 gói x 16 gam)",
+      price: "48.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/1639648313_ca-phe-sua-da-hoa-tan-dam-vi-viet_e99206e48a8f4e28b086556146165e05_large.jpg",
+      about: "Bắt đầu ngày mới với tách cà phê sữa “Đậm vị Việt” mạnh mẽ sẽ giúp bạn luôn tỉnh táo và hứng khởi cho ngày làm việc thật hiệu quả.",
+   },
+    { 
+      id: 'cafe10',
+      name: "Cà Phê Hoà Tan Đậm Vị Việt (18 gói x 16 gam)",
+      price: "99.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/1669707374_1642353251-ca-phe-dam-vi-viet-tui-new_8202f49602dd40ddac34ebb28e3145f8_large.jpg",
+      about: "Bắt đầu ngày mới với tách cà phê sữa “Đậm vị Việt” mạnh mẽ sẽ giúp bạn luôn tỉnh táo và hứng khởi cho ngày làm việc thật hiệu quả.",
    },
    { 
-      id: 'cafe08',
-      name: "cafe 08",
-        price: "38.000",
-      img: "https://product.hstatic.net/1000075078/product/1684482557_bg-product-1_e9409318c09d4ad4935d1554be363429_large.jpg"
+      id: 'cafe11',
+      name: "Cà Phê Hoà Tan Đậm Vị Việt (18 gói x 16 gam)",
+      price: "84.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/p6-lon-3in1_717216_97953b669769475382c5b157f80f9e2a_large.jpg",
+      about: "Với thiết kế lon cao trẻ trung, hiện đại và tiện lợi, Cà phê sữa đá lon thơm ngon đậm vị của The Coffee House sẽ đồng hành cùng nhịp sống sôi nổi của tuổi trẻ và giúp bạn có được một ngày làm việc đầy hứng khởi.",
    },
-   ]
+    { 
+      id: 'cafe12',
+      name: "Cà Phê Hoà Tan Đậm Vị Việt (18 gói x 16 gam)",
+      price: "336.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/24-lon-cpsd_225680_2e56ba358a00410395c7baa175810a77_large.jpg",
+      about: "Với thiết kế lon cao trẻ trung, hiện đại và tiện lợi, Cà phê sữa đá lon thơm ngon đậm vị của The Coffee House sẽ đồng hành cùng nhịp sống sôi nổi của tuổi trẻ và giúp bạn có được một ngày làm việc đầy hứng khởi.",
+   },
+   ];
+
+let teas = [
+{ 
+      id: 'tea01',
+      name: "Trà Vị Đào Tearoma 14x14g",
+      price: "32.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/1669706771_bg-tradao-min_0eb7a1ea0cd642c2b6885970f85e4938_large.jpg",
+      about: "Được chiết xuất từ 100% trà tự nhiên, không phẩm màu tổng hợp, Trà vị Đào Tearoma mang đến cảm giác thanh mát cực đã. Nhấp một ngụm, cảm nhận trọn vị đào chua ngọt thơm ngon bùng nổ. Ngoài ra, trà còn bổ sung vitamin C cực kỳ có lợi cho sức khoẻ.",
+   },
+   { 
+      id: 'tea02',
+      name: "Trà Sữa Trân Châu Tearoma 250g",
+      price: "38.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/1669880647_bg-trasua-min_ab66217f476e4b3295f8ad1c9e471f17_large.jpg",
+      about: "Chỉ 2 phút có ngay ly Trà sữa trân châu ngon chuẩn vị quán. Thơm vị trà, béo vị sữa, cùng trân châu thật giòn dai sật sật. Đặc biệt, đây còn là thức uống tốt cho sức khoẻ nhờ thành phần 100% chiết xuất trà tự nhiên, không chất hoá học.",
+   },
+   { 
+      id: 'tea03',
+      name: "Trà Vị Tắc Mật Ong Tearoma 14x14g",
+      price: "32.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/1669706748_bg-tratatmatong-min_f2459a58cd724c1a91155a90d5481765_large.jpg",
+      about: "Thổi bùng sảng khoái cùng Trà vị Tắc Mật Ong Tearoma không phẩm màu tổng hợp. Chiết xuất từ 100% trà tự nhiên, bổ sung vitamin C tốt cho sức khoẻ. Vị tắc chua chua hoà cùng mật ong ngọt dịu giúp bật vị giác, bừng vị mát tức thì.",
+   },
+   { 
+      id: 'tea04',
+      name: "Trà Oolong Túi Lọc Tearoma 20x2G",
+      price: "28.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/1639646968_tra-oolong-tui-loc-tearoma-20x2gr_6fcb0e4f8f654fd79ca2db0903760652_large.jpg",
+      about: "Trà Oolong túi lọc với mùi hương dịu nhẹ hoàn toàn từ tự nhiên, vị hậu ngọt, và hương thơm tinh tế. Trà túi lọc Tearoma tiện lợi để sử dụng tại văn phòng, tại nhà,... nhưng vẫn đảm bảo được chất lượng về hương trà tinh tế, vị trà đậm đà.",
+   },
+   { 
+      id: 'tea05',
+      name: "Trà Lài Túi Lọc Tearoma 20x2G",
+      price: "28.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/1639647075_tra-lai-tui-loc-tearoma-20x2gr_7226dd7a930c48a1830463edfb393481_large.jpg",
+      about: "Trà túi lọc Tearoma hương lài thơm tinh tế, thanh mát, trên nền trà xanh đậm đà khó quên. Trà túi lọc Tearoma tiện lợi để sử dụng tại văn phòng, tại nhà,.. nhưng vẫn đảm bảo được chất lượng về hương trà tinh tế, vị trà đậm đà.",
+   },
+{ 
+      id: 'tea06',
+      name: "Trà Sen Túi Lọc Tearoma 20x2G",
+      price: "28.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/1639648068_tra-sen-tui-loc-tearoma-20x2gr_6bfc6c11b6a64c6b8a29303ae53f562c_large.jpg",
+      about: "Trà túi lọc Tearoma hương sen tinh tế, thanh mát, trên nền trà xanh đậm đà khó quên. Trà túi lọc Tearoma tiện lợi để sử dụng tại văn phòng, tại nhà, đi du lịch,... nhưng vẫn đảm bảo được chất lượng về hương trà tinh tế, vị trà đậm đà.",
+   },{
+
+      id: 'tea07',
+      name: "Trà Đào Túi Lọc Tearoma 20x2G",
+      price: "28.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/1639646846_tra-dao-tui-loc-tearoma-20x2gr_51dba126e55c45b9a05b7dcbc706edaa_large.jpg",
+      about: "Trà túi lọc Tearoma hương đào với hương thơm tinh tế và hoàn toàn tự nhiên, cùng nền trà đen đậm vị khó quên. Trà túi lọc Tearoma tiện lợi để sử dụng tại văn phòng, tại nhà,.. nhưng vẫn đảm bảo được chất lượng về hương trà tinh tế, vị trà đậm đà.",
+   },
+   { 
+      id: 'tea01',
+      name: "Giftset Trà Tearoma",
+      price: "166.000 đ",
+      img: "https://product.hstatic.net/1000075078/product/1641440575_gift-set-tearoma-1_e793b698ba19496683d9e213b77a82c8_large.jpeg",
+      about: "Hộp quà tặng với 4 hộp trà túi lọc Tearoma các loại là món quà thật ý nghĩa cho những người thân yêu.",
+   },
+];
+
    const product = document.querySelectorAll('.products')
    product.forEach((x, i)=> {
      x.querySelector('img').setAttribute('src', cafes[i].img) 
      x.querySelector('img').setAttribute('id', cafes[i].id) 
      x.querySelector('.card-text').innerText = cafes[i].name
      x.querySelector('.price').innerText = cafes[i].price
+   })
+   const tratuilocproduct = document.querySelectorAll('.teaproducts')
+   tratuilocproduct.forEach((x, i)=> {
+     x.querySelector('img').setAttribute('src', teas[i].img) 
+     x.querySelector('img').setAttribute('id', teas[i].id) 
+     x.querySelector('.card-text').innerText = teas[i].name
+     x.querySelector('.price').innerText = teas[i].price
    })
 
 function goDetail(event){
--- home.js
+++ home.js
@@ -17,113 +17,140 @@ hide.addEventListener("click", function () {
 });
 
 //san pham best seller & new
-let pros = [
+let bests = [
   {
-    id: "pro01",
+    id: "best01",
     name: "Cà Phê Sữa Đá",
     price: "29.000 đ",
     img: "https://product.hstatic.net/1000075078/product/1669736835_ca-phe-sua-da_420319c0ee4d460a8dc5e287b951e405_large.png",
+    about:"",
   },
   {
-    id: "pro02",
+    id: "best02",
     name: "Trà Đào Cam Xả",
     price: "49.000 đ",
     img: "https://product.hstatic.net/1000075078/product/1669736819_tra-dao-cam-sa-da_ce5fc8b77e204b71b8c28880c220f422_large.png",
+    about:"",
   },
   {
-    id: "pro03",
+    id: "best03",
     name: "Trà Sữa OoLong Nướng Trân Châu",
     price: "55.000 đ",
     img: "https://product.hstatic.net/1000075078/product/1669736877_tra-sua-oolong-nuong-tran-chau_ee56caa33ce3432e99571c302f0d6498_large.png",
+    about:"",
   },
   {
-    id: "pro04",
+    id: "best04",
     name: "CloudFee Hạnh Nhân Nướng",
     price: "49.000 đ",
     img: "https://product.hstatic.net/1000075078/product/1675357918_cloudfee-hanh-nhan-nuong-min_5878cf79201a4742a022b038076b3c68_large.png",
+    about:"",
   },
   {
-    id: "pro05",
+    id: "best05",
     name: "Hi-Tea Yuzu Trân Châu",
     price: "49.000 đ",
     img: "https://product.hstatic.net/1000075078/product/1669736859_hi-tea-yuzu-tran-chau_f72f9f2bb0934cb998a6674ad3f9da95_large.png",
+    about:"",
   },
   {
-    id: "pro06",
+    id: "best06",
     name: "Hi-Tea Vải",
     price: "49.000 đ",
     img: "https://product.hstatic.net/1000075078/product/1669736893_hi-tea-vai_06cc9054fb7e4cf5a56649703b9a8a51_large.png",
+    about:"",
   },
   {
-    id: "pro07",
+    id: "best07",
     name: "Bánh Mì Gậy Gà Kim Quất",
     price: "25.000 đ",
     img: "https://product.hstatic.net/1000075078/product/1669737009_banh-mi-gay-ga-kim-quat_d8b14ee5a6714aa9add84af7f7ba223e_large.png",
+    about:"",
   },
   {
-    id: "pro08",
+    id: "best08",
     name: "Chà Bông Phô Mai",
     price: "35.000 đ",
     img: "https://product.hstatic.net/1000075078/product/1669736993_cha-bong-pho-mai_13d60efed56943649cb4122526feb85e_large.png",
+    about:"",
   },
+  
+];
+let news = [
   {
-    id: "pro09",
+    id: "new01",
     name: "Phin Sữa Tươi Bánh Flan",
     price: "49.000 đ",
     img: "https://product.hstatic.net/1000075078/product/1696220170_phin-sua-tuoi-banh-flan_3d3873099d1a4de08335edc70f1cccc4_large.jpg",
+    about: "",
   },
   {
-    id: "pro10",
+    id: "new02",
     name: "Trà Xanh Espresso Marble",
     price: "49.000 đ",
     img: "https://product.hstatic.net/1000075078/product/1696220139_tra-xanh-espresso-marble_6645d935c1b84d9aa4909c38a8560d6c_large.jpg",
+    about: "",
   },
   {
-    id: "pro11",
+    id: "new03",
     name: "Trà Xanh Latte",
     price: "45.000 đ",
     img: "https://product.hstatic.net/1000075078/product/1689778809_tx-latte_330c146458494687bb8ad69644fa3f91_large.jpg",
+    about: "",
   },
   {
-    id: "pro12",
+    id: "new04",
     name: "Trà Xanh Đưòng Đen",
     price: "55.000 đ",
     img: "https://product.hstatic.net/1000075078/product/1689778611_tx-duong-den_24fd0e886e7b4a7e97c84283932b6e2b_large.jpg",
+    about: "",
   },
   {
-    id: "pro13",
+    id: "new05",
     name: "Frosty Trà Xanh",
     price: "59.000 đ",
     img: "https://product.hstatic.net/1000075078/product/1689778505_tx-frosty_f68029bb7c88463cbb450f363eb6f0a4_large.jpg",
+    about: "",
   },
   {
-    id: "pro14",
+    id: "new06",
     name: "Frosty Phin-Gato",
     price: "55.000 đ",
     img: "https://product.hstatic.net/1000075078/product/1686021160_phin-gato-new_d3d5b5bd900d423c8548db3ca3518623_large.png",
+    about: "",
   },
   {
-    id: "pro15",
+    id: "new07",
     name: "Frosty Cà Phê Đường Đen",
     price: "59.000 đ",
     img: "https://product.hstatic.net/1000075078/product/1686021199_ca-phe-duong-den-new_ceddb459c0194acfabf609eb9fd85704_large.png",
+    about: "",
   },
   {
-    id: "pro16",
+    id: "new08",
     name: "Frosty Bánh Kem Dâu",
     price: "59.000 đ",
     img: "https://product.hstatic.net/1000075078/product/1686021305_banh-kem-dau-new_7dcebeab9c47491fa867284a810106e8_large.jpg",
+    about: "",
   },
 ];
-const product = document.querySelectorAll(".home");
-product.forEach((x, i) => {
-  x.querySelector("img").setAttribute("src", pros[i].img);
-  x.querySelector("img").setAttribute("alt", pros[i].name);
-  x.querySelector("img").setAttribute("id", pros[i].id);
-  x.querySelector(".item_name").innerHTML = pros[i].name;
-  x.querySelector(".price_product_item").innerHTML = pros[i].price;
+const bestproduct = document.querySelectorAll(".best");
+bestproduct.forEach((x, i) => {
+  x.querySelector("img").setAttribute("src", bests[i].img);
+  x.querySelector("img").setAttribute("alt", bests[i].name);
+  x.querySelector("img").setAttribute("id", bests[i].id);
+  x.querySelector(".item_name").innerHTML = bests[i].name;
+  x.querySelector(".price_product_item").innerHTML = bests[i].price;
+});
+const newproduct = document.querySelectorAll(".new");
+newproduct.forEach((x, i) => {
+  x.querySelector("img").setAttribute("src", news[i].img);
+  x.querySelector("img").setAttribute("alt", news[i].name);
+  x.querySelector("img").setAttribute("id", news[i].id);
+  x.querySelector(".item_name").innerHTML = news[i].name;
+  x.querySelector(".price_product_item").innerHTML = news[i].price;
 });
 
 function goDetail(event){
-     window.location = `./cafeDetail/cafeDetail01.html?id=${event.id}`;
+     window.location= `./cafeDetail/cafeDetail01.html?id=${event.id}`;
 }
